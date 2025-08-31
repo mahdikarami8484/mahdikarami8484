b8d6cc06b1bcee3a4cd3bfcc9afe0eae643dfcc5
MahdiKarami <67632452+mahdikarami8484@users.noreply.github.com>
2025-08-31 23:29:13 +0330

    add source link of projects

--- src/project.ts
+++ src/project.ts
@@ -18,50 +18,50 @@ const projects: Project[] = [
         title: "ModuWeb (C++ & Drogon Framework)",
         status: Status.Ongoing,
         description: "ModuWeb is a modular C++ backend project built using the Drogon framework. It demonstrates a Modular Monolith architecture with separate plugins for authentication, email notifications, and user management. This project is ideal for learning C++ web development, REST APIs, and plugin-based architecture in a structured and maintainable way.",
-        sourceLink: "#",
-        liveLink: "#"
+        sourceLink: "https://github.com/mahdikarami8484/ModuWeb",
+        liveLink: ""
     },
     {
         title: "Voicechat App (C# & WindowsForm & TCP)",
         status: Status.Completed,
         description: "A simple VoiceChat appliction in C# and NET. I used Windows Form for the user interface because this program is only for learning sockets and a simple example of VoiceChat and may have problems and errors in real practice. This project is in the form of two client and server programs and the logic of its operation is very simple and non-practical.",
-        sourceLink: "#",
-        liveLink: "#"
+        sourceLink: "https://github.com/mahdikarami8484/VoicechatApp",
+        liveLink: ""
     },
     {
         title: "Game DLL Injection (C++)",
         status: Status.Ongoing,
         description: "Structured DLL injection system for GTA: San Andreas with hooked rendering functions.",
-        sourceLink: "#",
-        liveLink: "#"
+        sourceLink: "https://github.com/mahdikarami8484/DoomDLL",
+        liveLink: ""
     },
     {
         title: "DigiNumber (Vue.js & Tailwind)",
         status: Status.Completed,
         description: "Frontend of a Telegram Mini-App for virtual SIM card store.",
-        sourceLink: "#",
-        liveLink: "#"
+        sourceLink: "https://github.com/mahdikarami8484/diginumber",
+        liveLink: "https://mahdikarami8484.github.io/diginumber/front-end/build"
     },
     {
         title: "Snake Game (C++ & windows.h)",
         status: Status.Completed,
         description: "A SnakeGame with cpp for windows.",
-        sourceLink: "#",
-        liveLink: "#"
+        sourceLink: "https://github.com/mahdikarami8484/SnakeGame",
+        liveLink: ""
     },
     {
         title: "Box Urls (PHP & Bootstrap)",
         status: Status.Completed,
         description: "A website for save and protect your urls and links.",
-        sourceLink: "#",
-        liveLink: "#"
+        sourceLink: "https://github.com/mahdikarami8484/Box-Urls",
+        liveLink: ""
     },
     {
         title: "Learn Vue ",
         status: Status.Completed,
         description: "Three projects for learning Vue.js",
-        sourceLink: "#",
-        liveLink: "#"
+        sourceLink: "https://github.com/mahdikarami8484/learn-vue",
+        liveLink: "https://mahdikarami8484.github.io/learn-vue/project3"
     },
 ];
 
@@ -82,8 +82,8 @@ export const renderProjects = () => {
           <p>${project.description}</p>
         </div>
         <div class="flex mt-auto justify-around p-2 w-full gap-6">
-          ${project.sourceLink ? `<a href="${project.sourceLink}" class="shadow shadow-gray-200 p-2 rounded-md text-gray-700 cursor-pointer hover:scale-105 hover:font-medium">Source</a>` : ""}
-          ${project.liveLink ? `<a href="${project.liveLink}" class="shadow shadow-teal-200 p-2 rounded-md text-white cursor-pointer bg-teal-300 hover:scale-105 hover:font-medium">Live View</a>` : ""}
+          ${project.sourceLink ? `<a target="_blank" href="${project.sourceLink}" class="shadow shadow-gray-200 p-2 rounded-md text-gray-700 cursor-pointer hover:scale-105 hover:font-medium">Source</a>` : ""}
+          ${project.liveLink ? `<a target="_blank" href="${project.liveLink}" class="shadow shadow-teal-200 p-2 rounded-md text-white cursor-pointer bg-teal-300 hover:scale-105 hover:font-medium">Live View</a>` : ""}
         </div>
       </div>
     `;
